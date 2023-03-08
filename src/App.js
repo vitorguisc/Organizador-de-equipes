ed6ed7573ca978a0e2f088758fb21604f070fdc8
Vitor <vitorguilherme@geb.inatel.br>
2023-03-08 15:54:54 -0300

    cadastro dos colaboradores

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
 
 function App() {
+
+  const [colaboradores, setColaboradore] = useState([])
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador)
+    setColaboradore([...colaboradores, colaborador])
+  }
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
     </div>
   );
 }
