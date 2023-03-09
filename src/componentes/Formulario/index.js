e544e92c3794fba98a584ac57606a1fa462569a9
Vitor <vitorguilherme@geb.inatel.br>
2023-03-09 10:10:24 -0300

    refatorando formulario/index.jsx e app.js

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -6,15 +6,7 @@ import { useState } from 'react'
 
 const Formulario = (props) => {
 
-    const times = [
-        'Programação',
-        'Front-End',
-        'Data Science',
-        'Devops',
-        'UX e Design',
-        'Mobile',
-        ' Inovação e Gestão'
-    ]
+   
     const [nome, setNome] = useState('')
     const [cargo, setCargo] = useState('')
     const [imagem, setImagem] = useState('')
@@ -54,7 +46,7 @@ const Formulario = (props) => {
                 <ListaSuspensa
                     obrigatorio={true}
                     label="Time"
-                    itens={times}
+                    itens={props.times}
                     valor={time}
                     aoAlterado={valor => setTime(valor)}
                 />
